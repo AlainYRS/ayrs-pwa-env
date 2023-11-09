ccc4359d6b82408fe22b89ddf015989229ef10fa
Alain Rivera <galain0106@gmail.com>
2023-11-09 16:04:16 -0500

    environment variables

--- app/firebaseCongif.js
+++ app/firebaseCongif.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXAvNWxghArXGOWczIVSZK5lFLoml6o1Q",
-  authDomain: "pwayrs-environment.firebaseapp.com",
-  projectId: "pwayrs-environment",
-  storageBucket: "pwayrs-environment.appspot.com",
-  messagingSenderId: "339862237374",
-  appId: "1:339862237374:web:fc0253e73fb5b2a7a66eef",
-  measurementId: "G-9MHYEFVS9B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
