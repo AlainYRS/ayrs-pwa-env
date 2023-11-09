87d6306634c89a77b3eba27f2a2d552e1dcf66d5
Alain Rivera <galain0106@gmail.com>
2023-11-09 17:30:16 -0500

    Deployment to Vercel succesfully to test in template production version

--- app/firebaseCongif.js
+++ app/firebaseCongif.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,6 +16,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
 export { db };
