fa59a81587510cb2b895959e9c502b41f06ef2d9
Alain Rivera <galain0106@gmail.com>
2023-11-09 22:01:54 -0500

    Google Analytics Implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import GoogleAnalytics from '@/components/GoogleAnalytics';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_ANALYTICS_ID}/>
       <body className={inter.className}>{children}</body>
     </html>
   )
---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Script from "next/script";
+
+export default function GoogleAnalytics({GA_MEASUREMENT_ID}: {GA_MEASUREMENT_ID: string | undefined}){
+    return(
+        <>
+            <Script strategy="afterInteractive" 
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}/>
+            <Script id={'google-analytics'} strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}',{
+                        page_path: window.location.pathname,
+                    });
+                    `,
+                }}
+            />
+        </>
+    );
+}
