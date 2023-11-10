1bb289c4028b64ef8fd0532e44502f54fe14ead0
Alain Rivera <galain0106@gmail.com>
2023-11-09 21:25:28 -0500

    Google Analytics implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title></title>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html:`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());            
+            gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}',{
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        >
+        </script>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
